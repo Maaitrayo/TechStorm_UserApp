c6cd35472f16184c6799b9b11b3b6635e69061bb
Maaitrayo Das <dasmaaitrayo@gmail.com>
2022-12-14 00:00:46 +0530

    initial commit

---
+++ app/src/main/java/com/example/techstorm223/NavigationItem.kt
@@ -0,0 +1,11 @@
+package com.example.techstorm223
+
+sealed class NavigationItem(var route: String, var icon: Int, var title: String)
+{
+    object Home : NavigationItem("home", R.drawable.ic_home, "Home")
+    object Profile : NavigationItem("profile", R.drawable.ic_person, "Profile")
+    object Settings : NavigationItem("settings", R.drawable.ic_settings, "Settings")
+    object Share : NavigationItem("share", R.drawable.ic_share, "Share")
+    object Contact : NavigationItem("contact", R.drawable.ic_contact, "Contact")
+    object Developers : NavigationItem("developers", R.drawable.ic_new, "Developers")
+}
